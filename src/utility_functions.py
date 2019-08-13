e6025c7956d268c3ef460ab863842442a3fbd3f4
ericguizzo <eric.guizzo@hotmail.it>
2019-08-13 16:41:06 +0100

    dummy job

--- src/utility_functions.py
+++ src/utility_functions.py
@@ -97,12 +97,12 @@ def build_matrix_dataset(merged_predictors, merged_target, actors_list):
     for i in actors_list:
         if i == actors_list[0]:  #if is first item
             predictors = np.array(merged_predictors[i])
-            target = np.array(merged_target[i],dtype='float32')
+            #target = np.array(merged_target[i],dtype='float32')
             #print (i, predictors.shape)
         else:
             if np.array(merged_predictors[i]).shape != (0,):  #if it not void due to preprocessing errors
                 predictors = np.concatenate((predictors, np.array(merged_predictors[i])), axis=0)
-                target = np.concatenate((target, np.array(merged_target[i],dtype='float32')), axis=0)
+                #target = np.concatenate((target, np.array(merged_target[i],dtype='float32')), axis=0)
         index += 1
         perc = int(index / total * 20)
         perc_progress = int(np.round((float(index)/total) * 100))
@@ -112,7 +112,7 @@ def build_matrix_dataset(merged_predictors, merged_target, actors_list):
     print(' | shape: ' + str(predictors.shape))
     print ('\n')
 
-    return predictors, target
+    return predictors
 
 def find_longest_audio(input_folder):
     '''
