03191c61ac5973b3070f8976bf8f15ab8623da7f
ericguizzo <eric.guizzo@hotmail.it>
2019-08-21 16:34:18 +0100

    added usethis scripts

--- src/prova.py
+++ src/prova.py
@@ -1,9 +1,11 @@
 import define_models as def_mod
 import numpy as np
+import torch
 
 encoder, p = def_mod.WAVE_encoder(1,1)
 
 input = np.random.rand(100)
+input = torch.tensor(input).cuda()
 
 y = encoder(input)
 print (y.shape)
