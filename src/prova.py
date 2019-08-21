254ec61ffc77d98f96de9d518b155aea47c083f7
ericguizzo <eric.guizzo@hotmail.it>
2019-08-21 17:15:21 +0100

    added usethis scripts

--- src/prova.py
+++ src/prova.py
@@ -9,17 +9,17 @@ encoder = encoder.to(device)
 decoder, p = def_mod.WAVE_decoder(1,1)
 decoder = decoder.to(device)
 
-
+'''
 input = np.random.rand(100)
 input = input.reshape(1,100)
 input = torch.tensor(input).float().to(device)
 y = encoder(input)
 print (y.shape)
-
 '''
+
+
 input1 = np.random.rand(16348)
 input1 = input1.reshape(1,16348)
 input1 = torch.tensor(input1).float().to(device)
 y1 = decoder(input1)
 print (y1.shape)
-'''
