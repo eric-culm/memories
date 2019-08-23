2935d9e017ceb771fec60bb9b378d2e489580088
ericguizzo <eric.guizzo@hotmail.it>
2019-08-23 17:05:53 +0100

    building vae model

--- src/training.py
+++ src/training.py
@@ -37,7 +37,11 @@ except IndexError:
     generator = True
     dataset = 'digits'
     architecture = 'WAVE_VAE'
+    encoder_architecture = 'WAVE_encoder'
+    decoder_architecture = 'WAVE_decoder'
+    reparametrize_architecture = 'reparametrize'
     parameters = ['verbose=True', 'model_size=64']
+    
     SAVE_MODEL = '../models/prova'
     results_path = '../results/provisional'
     parameters_path = results_path + '/parameters'
