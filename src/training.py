b5739b25da0b532f643910d90c5e158e48c5444f
ericguizzo <eric.guizzo@hotmail.it>
2019-09-02 17:24:53 +0100

    trying to get it work

--- src/training.py
+++ src/training.py
@@ -310,7 +310,7 @@ def main():
 
     #select a subdataset for testing (to be commented when normally trained)
 
-    bound = 5
+    bound = 50
     training_predictors = training_predictors[:bound]
     validation_predictors = validation_predictors[:bound]
     test_predictors = test_predictors[:bound]
