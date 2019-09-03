30910dd5c54314f5458335d26187af45d1c9bda4
ericguizzo <eric.guizzo@hotmail.it>
2019-09-03 11:30:43 +0100

    trying to get it work

--- src/training.py
+++ src/training.py
@@ -40,7 +40,7 @@ except IndexError:
     encoder_architecture = 'simple_encoder'
     decoder_architecture = 'simple_decoder'
     reparametrize_architecture = 'reparametrize'
-    parameters = ['verbose=False', 'model_size=64', 'variational=True',
+    parameters = ['verbose=False', 'model_size=64', 'variational=False',
                   'kld_weight=-0.5', 'warm_up=True']
 
     SAVE_MODEL = '../models/prova'
