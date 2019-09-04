1a2881aefff0ee0bd11d7d626121a470f058915d
ericguizzo <eric.guizzo@hotmail.it>
2019-09-04 13:20:47 +0100

    trying to get it work

--- src/training.py
+++ src/training.py
@@ -40,7 +40,7 @@ except IndexError:
     encoder_architecture = 'CNN_encoder'
     decoder_architecture = 'CNN_decoder'
     reparametrize_architecture = 'reparametrize'
-    parameters = ['verbose=False', 'model_size=64', 'variational=True',
+    parameters = ['verbose=False', 'model_size=64', 'variational=False',
                   'kld_weight=-0.5', 'warm_up=True', 'latent_dim=100']
 
     SAVE_MODEL = '../models/prova'
