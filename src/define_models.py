71a099ac9733137ef395bc2c4710f078227ba187
ericguizzo <eric.guizzo@hotmail.it>
2019-09-06 10:21:15 +0100

    trying to get it work

--- src/define_models.py
+++ src/define_models.py
@@ -750,6 +750,7 @@ def MNIST_encoder(time_dim, features_dim, user_parameters=['niente = 0']):
             print ('cul')
             print (x.shape)
             x = x.view(-1, 784)
+            print (x.shape)
             x = F.relu(self.bn1(self.fc1(x)))
             x = F.relu(self.bn2(self.fc2(x)))
 
