b83401539aca9514340a3ffb4ae648b2c1321cf2
ericguizzo <eric.guizzo@hotmail.it>
2019-09-06 15:25:31 +0100

    trying to get it work

--- src/training.py
+++ src/training.py
@@ -248,6 +248,7 @@ def main():
         training_predictors = training_predictors.reshape(training_predictors.shape[0], 1, training_predictors.shape[1],training_predictors.shape[2])
         validation_predictors = validation_predictors.reshape(validation_predictors.shape[0], 1, validation_predictors.shape[1], validation_predictors.shape[2])
         test_predictors = test_predictors.reshape(test_predictors.shape[0], 1, test_predictors.shape[1], test_predictors.shape[2])
+    '''
     else:
         training_predictors = training_predictors.reshape(training_predictors.shape[0], 1, training_predictors.shape[1])
         validation_predictors = validation_predictors.reshape(validation_predictors.shape[0], 1, validation_predictors.shape[1])
@@ -255,7 +256,7 @@ def main():
     training_target = training_target.reshape(training_target.shape[0], 1, training_target.shape[1])
     validation_predictors = validation_target.reshape(validation_target.shape[0], 1, validation_target.shape[1])
     test_predictors = test_target.reshape(test_target.shape[0], 1, test_target.shape[1])
-
+    '''
     #convert to tensor
     train_predictors = torch.tensor(training_predictors).float().to(device)
     val_predictors = torch.tensor(validation_predictors).float().to(device)
