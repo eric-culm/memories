9224ad7c9cecb15c0eeb11fca195adf2efee8494
ericguizzo <eric.guizzo@hotmail.it>
2019-09-06 15:33:58 +0100

    trying to get it work

--- src/define_models.py
+++ src/define_models.py
@@ -761,6 +761,8 @@ def spectrum_encoder(time_dim, features_dim, user_parameters=['niente = 0']):
         def forward(self, x):
 
             #features extraction
+            print ('cazzo')
+            print (x.shape)
             x = F.relu(self.conv1(x))
             x = self.pool1(x)
             x = F.relu(self.conv2(x))
