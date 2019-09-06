2d8dfad4105e28cc2ee29f2a0b16b4cb5dee7fa0
ericguizzo <eric.guizzo@hotmail.it>
2019-09-06 15:14:56 +0100

    trying to get it work

--- src/define_models.py
+++ src/define_models.py
@@ -732,34 +732,22 @@ def spectrum_encoder(time_dim, features_dim, user_parameters=['niente = 0']):
         def __init__(self, latent_dim=p['latent_dim'], variational=p['variational']):
             super(spectrum_encoder_class, self).__init__()
             self.variational = variational
-            self.features = nn.Sequential(
-            nn.Conv2d(1, 64, kernel_size=11, stride=4, padding=2),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.Conv2d(64, 192, kernel_size=5, padding=2),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.Conv2d(192, 384, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(384, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(256, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-            )
+            self.conv1 = nn.Conv2d(1, 64, kernel_size=11, stride=4, padding=2)
+            self.pool1 = nn.MaxPool2d(kernel_size=3, stride=2)
+            self.conv2 = nn.Conv2d(1, 64, kernel_size=11, stride=4, padding=2)
+            self.pool2 = nn.MaxPool2d(kernel_size=3, stride=2)
+            self.conv3 = nn.Conv2d(192, 384, kernel_size=3, padding=1)
+            self.conv4 = nn.Conv2d(384, 256, kernel_size=3, padding=1)
+            self.conv5 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
+            self.pool3 = nn.MaxPool2d(kernel_size=3, stride=2)
+
             self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
-            self.classifier = nn.Sequential(
-                nn.Dropout(),
-                nn.Linear(256 * 6 * 6, 4096),
-                nn.ReLU(inplace=True),
-                nn.Dropout(),
-                nn.Linear(4096, 4096),
-                nn.ReLU(inplace=True),
-                nn.Linear(4096, 2000),
-                nn.ReLU(inplace=True),
-                nn.Linear(2000, 500),
-                nn.ReLU(inplace=True)
-            )
+
+            self.fc1 = nn.Linear(256 * 6 * 6, 4096)
+            self.fc2 = nn.Linear(4096, 4096)
+            self.fc3 = nn.Linear(4096, 2000)
+            self.fc4 = nn.Linear(2000, 500)
+
 
             self.mu = nn.Linear(500, latent_dim)
             self.logvar = nn.Linear(500, latent_dim)
@@ -775,11 +763,24 @@ def spectrum_encoder(time_dim, features_dim, user_parameters=['niente = 0']):
         return x
 
         def forward(self, x):
+            #features extraction
+            x = F.relu(self.conv1(x))
+            x = self.pool1(x)
+            x = F.relu(self.conv2(x))
+            x = self.pool2(x)
+            x = F.relu(self.conv3(x))
+            x = F.relu(self.conv4(x))
+            x = F.relu(self.conv5(x))
+            x = self.pool3(x)
 
-            x = self.features(x)
             x = self.avgpool(x)
+
+            #classification
             x = torch.flatten(x, 1)
-            x = self.classifier(x)
+            x = F.relu(self.fc1(x))
+            x = F.relu(self.fc2(x))
+            x = F.relu(self.fc3(x))
+            x = F.relu(self.fc4(x))
 
             x1 = F.sigmoid(self.mu(x))
             if self.variational:
