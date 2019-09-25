a3c4c964f0c2f859410a40172d908a4b5e55fe22
ericguizzo <eric.guizzo@hotmail.it>
2019-09-25 15:49:24 +0100

    added latent distribution plot

--- src/training.py
+++ src/training.py
@@ -293,9 +293,9 @@ def main():
 
     #reshape tensors
     if features_type == 'waveform':
-        training_predictors = training_predictors.reshape(training_predictors.shape[0], 1, training_predictors.shape[1])
-        validation_predictors = validation_predictors.reshape(validation_predictors.shape[0], 1, validation_predictors.shape[1])
-        test_predictors = test_predictors.reshape(test_predictors.shape[0], 1, test_predictors.shape[1])
+        training_predictors = training_predictors.reshape(training_predictors.shape[0], 1, training_predictors.shape[2])
+        validation_predictors = validation_predictors.reshape(validation_predictors.shape[0], 1, validation_predictors.shape[2])
+        test_predictors = test_predictors.reshape(test_predictors.shape[0], 1, test_predictors.shape[2])
     elif features_type =='spectrum':
         training_predictors = training_predictors.reshape(training_predictors.shape[0], 1, training_predictors.shape[1],training_predictors.shape[2])
         validation_predictors = validation_predictors.reshape(validation_predictors.shape[0], 1, validation_predictors.shape[1], validation_predictors.shape[2])
