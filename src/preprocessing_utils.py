0a990bd0413d215cf175e69de97f39b610a4f88c
ericguizzo <eric.guizzo@hotmail.it>
2019-09-27 16:00:45 +0100

    yeah

--- src/preprocessing_utils.py
+++ src/preprocessing_utils.py
@@ -276,8 +276,8 @@ def preprocess_foldable_item(sounds_list, max_file_length, get_label_function, p
                         #print ('Foldable item progress:')
         except Exception as e:
             #print ('\r corrupted file found: not added to dataset')
-            #print (e)
-            pass
+            print (e)
+            break
 
         index_ += 1
         if print_item_progress:
