81890813efb16f1bf52a7824500a18a3d502a0a3
ericguizzo <eric.guizzo@hotmail.it>
2019-10-02 14:11:08 +0100

    cul

--- src/sbatch_job.sh
+++ src/sbatch_job.sh
@@ -1,13 +1,12 @@
 #! /bin/bash
 
 SBATCH --job-name="training"
-#SBATCH --mail-type=ALL
 SBATCH --nodes=1
 SBATCH --ntasks-per-node=8
 SBATCH --output=PROVAjob%J.out
 SBATCH --error=PROVAjob%J.err
 SBATCH --partition=normal
-SBATCH --gres=gpu:1
+SBATCH --gres=gpu:0
 
 module load cuda/9.2
 
