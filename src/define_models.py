897f3488aab5fd939b627894e6260489a7056c32
ericguizzo <eric.guizzo@hotmail.it>
2019-10-06 11:49:39 +0100

    merda

--- src/define_models.py
+++ src/define_models.py
@@ -487,7 +487,7 @@ def WAVE_complete_net(time_dim, features_dim, user_parameters=['niente = 0']):
             #So here we will first define layers for encoder network
             self.encoder = nn.Sequential(nn.Linear(16384,2000),
                                         nn.BatchNorm1d(2000),
-                                         F.relu(),
+                                         nn.ReLU(),
                                          nn.Linear(2000,2000),
                                          nn.BatchNorm1d(2000),
                                          F.relu(),
