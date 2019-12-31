d40fdd0f0488985eed2e404c3715b875b1ac504d
eric <eric@erics-MacBook-Pro-2.local>
2019-12-31 15:41:12 +0100

    ridicolo

--- src/samplernn_wrapper.py
+++ src/samplernn_wrapper.py
@@ -1,4 +1,5 @@
 import os
+import utility_functions as uf
 import subprocess
 import configparser
 import loadconfig
@@ -14,6 +15,10 @@ SRNN_CODE_PATH = cfg.get('samplernn', 'samplernn_code_path')
 SRNN_ENV_PATH = cfg.get('samplernn', 'samplernn_env_path')
 
 def split_audio(input_file, chunk_size=SRNN_CHUNK_SIZE, sr=SRNN_SR):
+    '''
+    load audio file, convert it to wav with given sr and splice it
+    in equal chunks
+    '''
     dataset_name = os.path.basename(input_file).split('.')[0]
     dataset_name = dataset_name + '_' + str(chunk_size) + '_' + str(sr)
     split_script = SRNN_DATASET_PATH + '/split_file.sh'
@@ -26,5 +31,42 @@ def split_audio(input_file, chunk_size=SRNN_CHUNK_SIZE, sr=SRNN_SR):
     process.communicate()
     process.wait()
 
-def train_srnn(code_path=SRNN_CODE_PATH, env_path=SRNN_ENV_PATH):
-    conda_string = 'conda run -p ' + env_path
+
+def split_all_files(input_folder, chunk_size=SRNN_CHUNK_SIZE, sr=SRNN_SR):
+    '''
+    split all audio files in a folder
+    '''
+    contents = os.listdir(input_folder)
+    contents = [os.path.join(input_folder, x) for x in contents]
+    i = 0
+    for sound in contents:
+        split_audio(sound)
+        uf.print_bar(i, len(contents))
+        i += 1
+
+def train_srnn(input_dataset, frame_sizes='16 4', n_rnn=2, batch_size=128, keep_old_checkpoints=False,
+               epoch_limit=1000, resume=True, sample_rate=SRNN_SR, n_samples=1,
+               sample_length=2, sampling_temperature=0.95, env_path=SRNN_ENV_PATH):
+    '''
+    wrapper for SampleRNN training
+    '''
+    exp_name = os.path.basename(input_dataset).split('.')[0]
+    sample_length = sample_length * sample_rate
+    conda_string = 'conda run -p ' + str(env_path)
+    train_string = ' python train.py' + \
+                   ' --exp ' + str(exp_name) + \
+                   ' --frame_sizes ' + str(frame_sizes) + \
+                   ' --n_rnn ' + str(n_rnn) + \
+                   ' --batch_size ' + str(batch_size) + \
+                   ' --keep_old_checkpoints ' + str(keep_old_checkpoints) + \
+                   ' --epoch_limit ' + str(epoch_limit) + \
+                   ' --resume ' + str(resume) + \
+                   ' --sample_rate ' + str(sample_rate) + \
+                   ' --n_samples ' + str(n_samples) + \
+                   ' --sample_length ' + str(sample_length) + \
+                   ' --sampling_temperature ' + str(sampling_temperature) + \
+                   ' --dataset ' + str(input_dataset)
+    command = conda_string + train_string
+    training = subprocess.Popen(command, shell=True)
+    training.communicate()
+    training.wait()
