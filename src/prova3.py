bae140492a9d9064af0a53ce0184dd6c0ed161b4
eric <eric@erics-MacBook-Pro-2.local>
2019-12-31 17:29:40 +0100

    ridicolo

--- src/prova3.py
+++ src/prova3.py
@@ -1,13 +1,2 @@
-import numpy as np
-import matplotlib.pyplot as plt
 
-input = np.arange(100) / 100
-
-input = input * 2
-
-bound = 8
-
-input = input ** np.sqrt(bound)
-
-plt.plot(input)
-plt.show()
+print ('vaffanculo bastardo')
--- src/samplernn_wrapper.py
+++ src/samplernn_wrapper.py
@@ -56,6 +56,39 @@ def train_srnn(input_dataset, frame_sizes='16 4', n_rnn=2, batch_size=128, keep_
     #exp_name = os.path.basename(input_dataset).split('.')[0]
     exp_name = input_dataset
     sample_length = sample_length * sample_rate
+    command =     ['conda run -n ', str(env_name),
+                   'python train.py',
+                   '--exp', str(exp_name),
+                   '--frame_sizes', str(frame_sizes),
+                   '--n_rnn', str(n_rnn),
+                   '--batch_size', str(batch_size),
+                   '--keep_old_checkpoints', str(keep_old_checkpoints),
+                   '--epoch_limit', str(epoch_limit),
+                   '--resume', str(resume),
+                   '--sample_rate', str(sample_rate),
+                   '--n_samples', str(n_samples),
+                   '--sample_length', str(sample_length),
+                   '--sampling_temperature', str(sampling_temperature),
+                   '--dataset ', str(input_dataset)]
+    #command = conda_string + train_string
+    print (command)
+    training = subprocess.Popen(command, shell=True, cwd=code_path)
+    training.communicate()
+    training.wait()
+
+
+def train_srnn_copy(input_dataset, frame_sizes='16 4', n_rnn=2, batch_size=128, keep_old_checkpoints=False,
+               epoch_limit=1000, resume=True, sample_rate=SRNN_SR, n_samples=1,
+               sample_length=2, sampling_temperature=0.95, env_name=SRNN_ENV_NAME,
+               code_path=SRNN_CODE_PATH):
+    '''
+    wrapper for SampleRNN training
+    '''
+    if input_dataset[-1] == '/':
+        input_dataset = input_dataset[:-1]
+    #exp_name = os.path.basename(input_dataset).split('.')[0]
+    exp_name = input_dataset
+    sample_length = sample_length * sample_rate
     conda_string = 'conda run -n ' + str(env_name)
     train_string = ' python train.py' + \
                    ' --exp ' + '"' + str(exp_name) + '"' + \
