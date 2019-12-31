90706bc3dcf3015407acc9963fdbe72e90be6953
eric <eric@erics-MacBook-Pro-2.local>
2019-12-31 16:34:16 +0100

    ridicolo

--- src/samplernn_wrapper.py
+++ src/samplernn_wrapper.py
@@ -73,6 +73,6 @@ def train_srnn(input_dataset, frame_sizes='16 4', n_rnn=2, batch_size=128, keep_
                    ' --dataset ' + str(input_dataset)
     command = cd_string + conda_string + train_string
     print (command)
-    training = subprocess.Popen(command)
-    training.communicate()
+    training = subprocess.Popen(command, shell=True)
+    training.call()
     training.wait()
