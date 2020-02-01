b06b860b40d70281a0cc39257ae2c8863467715c
eric <eric.guizzo@hotmail.it>
2020-02-01 14:35:59 +0000

    rotturadicazzo

--- src/scene_constrains.py
+++ src/scene_constrains.py
@@ -1,8 +1,12 @@
 import numpy as np
 import sys
 import copy
+import pprint
+import random
 
 constrains_dict = {'sound':{},'score':{}}
+parameters = {}
+
 '''
 constrains_dict['sound'] = {'category': [],
                                'model': [],
@@ -30,43 +34,84 @@ def get_constrains(constrains_list):
     join all requested constrains in one dict
     '''
     output_dict = copy.deepcopy(constrains_dict)
-    for constrain in constrains_list:
-        curr_constrain = eval(constrain)
+    for curr_constrain in constrains_list:
+        #curr_constrain = parameters[constrain]
         for key in curr_constrain.keys():
             for param in curr_constrain[key].keys():
                 curr_value = curr_constrain[key][param]
                 output_dict[key][param] = curr_value
     return output_dict
 
+def printTree(t,s):
+    a = 0
+    if not isinstance(t,dict) and not isinstance(t,list):
+        print ("\t"*s+str(t))
+    else:
+        for key in t:
+            if key != 'score' and key != 'sound':
+                print ("\t"*s+str(key))
+                if not isinstance(t,list):
+                    printTree(t[key],s+1)
+                    a += 1
+
+def gen_random_macro():
+    p_list = []
+    tot_params = len(list(parameters.keys())) - 1  #-1 subtracts the macro category
+    min_params = 4
+    num_params = np.arange(min_params,tot_params)
+    num_params = np.random.choice(num_params)  #random number of constrains
+    keys = list(parameters.keys())
+    random.shuffle(keys)
+
+    print ('random constrains:')
+    for i in range(num_params):
+        curr_key = parameters[keys[i]]
+        options = list(parameters[keys[i]].keys())
+        sel_option = np.random.choice(options)
+        p_list.append(parameters[keys[i]][sel_option])
+        print (str(keys[i]) + ': ' + str(sel_option))
+
+    output_dict = get_constrains(p_list)
+
+    return output_dict
+
+
+
+
+#SOUND SELECTION CONSTRAINS
+parameters['selection_availability'] = {}
+parameters['selection_type'] = {}
+parameters['selection_length'] = {}
+
+
 #only available
 #only_available = constrains_dict.copy()
-only_available = copy.deepcopy(constrains_dict)
-only_available['sound']['category'] = lambda x: ['instrumental']
-only_available['sound']['model'] = lambda x: ['africanPercs', 'ambient1', 'buchla',
+parameters['selection_availability']['only_available'] = copy.deepcopy(constrains_dict)
+parameters['selection_availability']['only_available']['sound']['category'] = lambda x: ['instrumental']
+parameters['selection_availability']['only_available']['sound']['model'] = lambda x: ['africanPercs', 'ambient1', 'buchla',
                                             'buchla2', 'classical', 'classical2',
                                             'guitarAcoustic', 'jazz', 'organ', 'percsWar',
                                             'percussions', 'pianoChill']
 
 
-#SOUND SELECTION CONSTRAINS
 #prefer highest quality
-prefer_hq = copy.deepcopy(constrains_dict)
+parameters['selection_type']['more_hq'] = copy.deepcopy(constrains_dict)
 def phq(in_list):
     for i in range(len(in_list)):
         in_list.append(0)
     return in_list
-prefer_hq['sound']['variation'] = phq
+parameters['selection_type']['more_hq']['sound']['variation'] = phq
 
 #prefer lowest quality
-prefer_lq = copy.deepcopy(constrains_dict)
+parameters['selection_type']['more_lq'] = copy.deepcopy(constrains_dict)
 def plq(in_list):
     for i in range(len(in_list)):
         list.append(max(in_list))
     return in_list
-prefer_lq['sound']['variation'] = phq
+parameters['selection_type']['more_lq']['sound']['variation'] = phq
 
 #prefer mid quality
-prefer_mq = copy.deepcopy(constrains_dict)
+parameters['selection_type']['more_mq'] = copy.deepcopy(constrains_dict)
 def phmq(in_list):
     to_append = in_list.copy()
     to_append.remove(max(to_append))
@@ -74,222 +119,245 @@ def phmq(in_list):
     for i in range(len(list)):
         in_list.append(to_append)
     return in_list
-prefer_mq['sound']['variation'] = phq
+parameters['selection_type']['more_mq']['sound']['variation'] = phq
 
 #only long sounds selected
-only_long_selected = copy.deepcopy(constrains_dict)
-only_long_selected['sound']['dur'] = lambda x: [60,60,30]
+parameters['selection_length'] = {}
+parameters['selection_length']['long'] = copy.deepcopy(constrains_dict)
+parameters['selection_length']['long']['sound']['dur'] = lambda x: [60,60,30]
 
 #only short sounds selected
-only_short_selected = copy.deepcopy(constrains_dict)
-only_short_selected['sound']['dur'] = lambda x: [10,5,3]
+parameters['selection_length']['short'] = copy.deepcopy(constrains_dict)
+parameters['selection_length']['short']['sound']['dur'] = lambda x: [10,5,3]
 
 #SCORE CONSTRAINS
-
+parameters['length'] = {}
 #only sounds longer than 40 sec
-very_long_scored = copy.deepcopy(constrains_dict)
-very_long_scored['score']['dur'] = lambda x: np.arange(40,60,0.1)
+parameters['length']['very_long'] = copy.deepcopy(constrains_dict)
+parameters['length']['very_long']['score']['dur'] = lambda x: np.arange(40,60,0.1)
 
 #only sounds between 10 and 40
-long_scored = copy.deepcopy(constrains_dict)
-long_scored['score']['dur'] = lambda x: np.arange(10,40,0.01)
+parameters['length']['long'] = copy.deepcopy(constrains_dict)
+parameters['length']['long']['score']['dur'] = lambda x: np.arange(10,40,0.01)
 
 #only sounds between 5,20
-short_scored = copy.deepcopy(constrains_dict)
-short_scored['score']['dur'] = lambda x: np.arange(1,10,0.01)
+parameters['length']['short'] = copy.deepcopy(constrains_dict)
+parameters['length']['short']['score']['dur'] = lambda x: np.arange(1,10,0.01)
 
 #only sounds between 0.2 and 1
-very_short_scored = copy.deepcopy(constrains_dict)
-very_short_scored['score']['dur'] = lambda x: np.arange(0.2,1,0.01)
+parameters['length']['very_short'] = copy.deepcopy(constrains_dict)
+parameters['length']['very_short']['score']['dur'] = lambda x: np.arange(0.2,1,0.01)
 
+#VOLUME
+parameters['volume'] = {}
 #hi volume
-volume_hi = copy.deepcopy(constrains_dict)
-volume_hi['score']['volume'] = lambda x: np.arange(0.7,1,0.01)
+parameters['volume']['high'] = copy.deepcopy(constrains_dict)
+parameters['volume']['high']['score']['volume'] = lambda x: np.arange(0.7,1,0.01)
 
 #mid volume
-volume_mid = copy.deepcopy(constrains_dict)
-volume_mid['score']['volume'] = lambda x: np.arange(0.3,0.7,0.01)
+parameters['volume']['mid'] = copy.deepcopy(constrains_dict)
+parameters['volume']['mid']['score']['volume'] = lambda x: np.arange(0.3,0.7,0.01)
 
 #low volume
-volume_low = copy.deepcopy(constrains_dict)
-volume_low['score']['volume'] = lambda x: np.arange(0.1,0.3,0.01)
+parameters['volume']['low'] = copy.deepcopy(constrains_dict)
+parameters['volume']['low']['score']['volume'] = lambda x: np.arange(0.1,0.3,0.01)
+
+#POSITION
+parameters['position'] = {}
+#position beginning
+parameters['position']['at_beginning'] = copy.deepcopy(constrains_dict)
+parameters['position']['at_beginning']['score']['position'] = lambda x: [0]
 
 #position beginning
-at_beginning = copy.deepcopy(constrains_dict)
-at_beginning['score']['position'] = lambda x: [0]
-
-#position beginning
-pos_initial = copy.deepcopy(constrains_dict)
-pos_initial['score']['position'] = lambda x: np.arange(0,0.3,0.01)
+parameters['position']['initial'] = copy.deepcopy(constrains_dict)
+parameters['position']['initial']['score']['position'] = lambda x: np.arange(0,0.3,0.01)
 
 #position mid
-pos_mid = copy.deepcopy(constrains_dict)
-pos_mid['score']['position'] = lambda x: np.arange(0.3,0.7,0.01)
+parameters['position']['mid'] = copy.deepcopy(constrains_dict)
+parameters['position']['mid']['score']['position'] = lambda x: np.arange(0.3,0.7,0.01)
 
 #position ending
-pos_ending = copy.deepcopy(constrains_dict)
-pos_ending['score']['position'] = lambda x: np.arange(0.7,1.,0.01)
+parameters['position']['ending'] = copy.deepcopy(constrains_dict)
+parameters['position']['ending']['score']['position'] = lambda x: np.arange(0.7,1.,0.01)
 
+#PAN
+parameters['pan'] = {}
 #around center
-pan_around_center = copy.deepcopy(constrains_dict)
-pan_around_center['score']['pan'] = lambda x: np.arange(-0.2,0.2,0.01)
+parameters['pan']['around_center'] = copy.deepcopy(constrains_dict)
+parameters['pan']['around_center']['score']['pan'] = lambda x: np.arange(-0.2,0.2,0.01)
 
 #towards left
-pan_towards_left = copy.deepcopy(constrains_dict)
-pan_towards_left['score']['pan'] = lambda x: np.arange(-0.2,-1,0.01)
+parameters['pan']['towards_left'] = copy.deepcopy(constrains_dict)
+parameters['pan']['towards_left']['score']['pan'] = lambda x: np.arange(-0.2,-1,0.01)
 
 #towards right
-pan_towards_right = copy.deepcopy(constrains_dict)
-pan_towards_right['score']['pan'] = lambda x: np.arange(0.2,1,0.01)
+parameters['pan']['towards_right'] = copy.deepcopy(constrains_dict)
+parameters['pan']['towards_right']['score']['pan'] = lambda x: np.arange(0.2,1,0.01)
 
 #always left
-pan_always_left = copy.deepcopy(constrains_dict)
-pan_always_left['score']['pan'] = lambda x: [-1]
+parameters['pan']['always_left'] = copy.deepcopy(constrains_dict)
+parameters['pan']['always_left']['score']['pan'] = lambda x: [-1]
 
 #always right
-pan_always_right = copy.deepcopy(constrains_dict)
-pan_always_right['score']['pan'] = lambda x: [1]
+parameters['pan']['always_right'] = copy.deepcopy(constrains_dict)
+parameters['pan']['always_right']['score']['pan'] = lambda x: [1]
 
+#EQ
+parameters['eq'] = {}
 #eq always on
-always_eq = copy.deepcopy(constrains_dict)
-always_eq['score']['eq'] = lambda x: [True]
+parameters['eq']['always'] = copy.deepcopy(constrains_dict)
+parameters['eq']['always']['score']['eq'] = lambda x: [True]
 
 #more probable eq
-more_eq = copy.deepcopy(constrains_dict)
-more_eq['score']['eq'] = lambda x: [True, True, False]
+parameters['eq']['more'] = copy.deepcopy(constrains_dict)
+parameters['eq']['more']['score']['eq'] = lambda x: [True, True, False]
 
 #eq always on
-never_eq = copy.deepcopy(constrains_dict)
-never_eq['score']['eq'] = lambda x: [False]
+parameters['eq']['never'] = copy.deepcopy(constrains_dict)
+parameters['eq']['never']['score']['eq'] = lambda x: [False]
 
 #more probable not eq
-less_eq = copy.deepcopy(constrains_dict)
-less_eq['score']['eq'] = lambda x: [False, True, False]
+parameters['eq']['less'] = copy.deepcopy(constrains_dict)
+parameters['eq']['less']['score']['eq'] = lambda x: [False, True, False]
 
+#REV
+parameters['rev_prob'] = {}
+parameters['rev_length'] = {}
 #rev always on
-always_rev = copy.deepcopy(constrains_dict)
-always_rev['score']['rev'] = lambda x: [True]
+parameters['rev_prob'] = {}
+parameters['rev_prob']['always'] = copy.deepcopy(constrains_dict)
+parameters['rev_prob']['always']['score']['rev'] = lambda x: [True]
 
 #more probable rev
-more_rev = copy.deepcopy(constrains_dict)
-more_rev['score']['rev'] = lambda x: [True, True, False]
+parameters['rev_prob']['more'] = copy.deepcopy(constrains_dict)
+parameters['rev_prob']['more']['score']['rev'] = lambda x: [True, True, False]
 
 #rev always on
-never_rev = copy.deepcopy(constrains_dict)
-never_rev['score']['rev'] = lambda x: [False]
+parameters['rev_prob']['never'] = copy.deepcopy(constrains_dict)
+parameters['rev_prob']['never']['score']['rev'] = lambda x: [False]
 
 #more probable not rev
-less_rev = copy.deepcopy(constrains_dict)
-less_rev['score']['rev'] = lambda x: [False, True, False]
+parameters['rev_prob']['less'] = copy.deepcopy(constrains_dict)
+parameters['rev_prob']['less']['score']['rev'] = lambda x: [False, True, False]
 
 #always long rev
-always_long_rev = copy.deepcopy(constrains_dict)
-always_long_rev['score']['rev'] = lambda x: sorted(x)[-2:]
+parameters['rev_length'] = {}
+parameters['rev_length']['long'] = copy.deepcopy(constrains_dict)
+parameters['rev_length']['long']['score']['rev'] = lambda x: sorted(x)[-2:]
 
 #always short rev
-always_short_rev = copy.deepcopy(constrains_dict)
-always_short_rev['score']['rev'] = lambda x: sorted(x)[:2]
+parameters['rev_length']['short'] = copy.deepcopy(constrains_dict)
+parameters['rev_length']['short']['score']['rev'] = lambda x: sorted(x)[:2]
 
 #always mid reverb length
 def rmm(in_list):
     in_list.remove(min(in_list))
     in_list.remove(max(in_list))
     return in_list
-always_mid_rev = copy.deepcopy(constrains_dict)
-always_mid_rev['score']['rev'] = rmm
+parameters['rev_length']['mid'] = copy.deepcopy(constrains_dict)
+parameters['rev_length']['mid']['score']['rev'] = rmm
 
+#SEGMENT
+parameters['segment'] = {}
 #segment always on
-always_segment = copy.deepcopy(constrains_dict)
-always_segment['score']['segment'] = lambda x: [True]
+parameters['segment']['always'] = copy.deepcopy(constrains_dict)
+parameters['segment']['always']['score']['segment'] = lambda x: [True]
 
 #more probable segment
-more_segment = copy.deepcopy(constrains_dict)
-more_segment['score']['segment'] = lambda x: [True, True, False]
+parameters['segment']['more'] = copy.deepcopy(constrains_dict)
+parameters['segment']['more']['score']['segment'] = lambda x: [True, True, False]
 
 #segment always on
-never_segment = copy.deepcopy(constrains_dict)
-never_segment['score']['segment'] = lambda x: [False]
+parameters['segment']['never'] = copy.deepcopy(constrains_dict)
+parameters['segment']['never']['score']['segment'] = lambda x: [False]
 
 #more probable not segment
-less_segment = copy.deepcopy(constrains_dict)
-less_segment['score']['segment'] = lambda x: [False, True, False]
+parameters['segment']['less'] = copy.deepcopy(constrains_dict)
+parameters['segment']['less']['score']['segment'] = lambda x: [False, True, False]
 
+#STRETCH
+parameters['stretch'] = {}
 #stretch long
-stretch_long = copy.deepcopy(constrains_dict)
-stretch_long['score']['stretch'] = lambda x: [y for y in x if y>4]
+parameters['stretch']['long'] = copy.deepcopy(constrains_dict)
+parameters['stretch']['long']['score']['stretch'] = lambda x: [y for y in x if y>4]
 
 #stretch short
-stretch_short = copy.deepcopy(constrains_dict)
-stretch_short['score']['stretch'] = lambda x: [y for y in x if y<0.3]
-
-#stretch long
-stretch_long = copy.deepcopy(constrains_dict)
-stretch_long['score']['stretch'] = lambda x: [y for y in x if y>4]
+parameters['stretch']['short'] = copy.deepcopy(constrains_dict)
+parameters['stretch']['short']['score']['stretch'] = lambda x: [y for y in x if y<0.3]
 
 #stretch mid
-stretch_mid = copy.deepcopy(constrains_dict)
-stretch_mid['score']['stretch'] = lambda x: [y for y in x if y>0.5 and y<2]
+parameters['stretch']['mid'] = copy.deepcopy(constrains_dict)
+parameters['stretch']['mid']['score']['stretch'] = lambda x: [y for y in x if y>0.5 and y<2]
 
 #no stretch
-no_stretch = copy.deepcopy(constrains_dict)
-no_stretch['score']['stretch'] = lambda x: [1]
+parameters['stretch']['never'] = copy.deepcopy(constrains_dict)
+parameters['stretch']['never']['score']['stretch'] = lambda x: [1]
 
+#SHIFT
+parameters['shift'] = {}
 #shift lo
-shift_low = copy.deepcopy(constrains_dict)
-shift_low['score']['shift'] = lambda x: [y for y in x if y<30]
+parameters['shift']['low'] = copy.deepcopy(constrains_dict)
+parameters['shift']['low']['score']['shift'] = lambda x: [y for y in x if y<30]
 
 #shift hi
-shift_hi = copy.deepcopy(constrains_dict)
-shift_hi['score']['shift'] = lambda x: [y for y in x if y>12]
+parameters['shift']['high'] = copy.deepcopy(constrains_dict)
+parameters['shift']['high']['score']['shift'] = lambda x: [y for y in x if y>12]
 
 #shift mid
-shift_mid = copy.deepcopy(constrains_dict)
-shift_mid['score']['shift'] = lambda x: [y for y in x if y>-10 and y<10]
+parameters['shift']['mid'] = copy.deepcopy(constrains_dict)
+parameters['shift']['mid']['score']['shift'] = lambda x: [y for y in x if y>-10 and y<10]
 
 #no shift
-no_shift = copy.deepcopy(constrains_dict)
-no_shift['score']['shift'] = lambda x: [1]
+parameters['shift']['never'] = copy.deepcopy(constrains_dict)
+parameters['shift']['never']['score']['shift'] = lambda x: [1]
 
+#FADES
+parameters['fade_in'] = {}
+parameters['fade_out'] = {}
 #short fade in
-fade_in_short = copy.deepcopy(constrains_dict)
-fade_in_short['score']['fade_in'] = lambda x: np.arange(0,50,0.1)
+parameters['fade_in']['short'] = copy.deepcopy(constrains_dict)
+parameters['fade_in']['short']['score']['fade_in'] = lambda x: np.arange(0,50,0.1)
 
 #short fade out
-fade_out_short = copy.deepcopy(constrains_dict)
-fade_out_short['score']['fade_out'] = lambda x: np.arange(0,50,0.1)
+parameters['fade_out']['short'] = copy.deepcopy(constrains_dict)
+parameters['fade_out']['short']['score']['fade_out'] = lambda x: np.arange(0,50,0.1)
 
 #long fade in
-fade_in_long = copy.deepcopy(constrains_dict)
-fade_in_long['score']['fade_in'] = lambda x: np.arange(max(x)*0.7, max(x), 0.1)
+parameters['fade_in']['long'] = copy.deepcopy(constrains_dict)
+parameters['fade_in']['long']['score']['fade_in'] = lambda x: np.arange(max(x)*0.7, max(x), 0.1)
 
 #long fade out
-fade_out_long = copy.deepcopy(constrains_dict)
-fade_out_long['score']['fade_out'] = lambda x: np.arange(max(x)*0.7, max(x), 0.1)
+parameters['fade_out']['long'] = copy.deepcopy(constrains_dict)
+parameters['fade_out']['long']['score']['fade_out'] = lambda x: np.arange(max(x)*0.7, max(x), 0.1)
 
 #mid fade in
-fade_in_mid = copy.deepcopy(constrains_dict)
-fade_in_mid['score']['fade_in'] = lambda x: np.arange(max(x)*0.3, max(x)*0.7, 0.1)
+parameters['fade_in']['mid'] = copy.deepcopy(constrains_dict)
+parameters['fade_in']['mid']['score']['fade_in'] = lambda x: np.arange(max(x)*0.3, max(x)*0.7, 0.1)
 
 #mid fade out
-fade_out_mid = copy.deepcopy(constrains_dict)
-fade_out_mid['score']['fade_out'] = lambda x: np.arange(max(x)*0.3, max(x)*0.7, 0.1)
+parameters['fade_out']['mid'] = copy.deepcopy(constrains_dict)
+parameters['fade_out']['mid']['score']['fade_out'] = lambda x: np.arange(max(x)*0.3, max(x)*0.7, 0.1)
 
 
 
 #MACROS
 #combinations of constrains
+parameters['macro'] = {}
+p = parameters.copy()
 
 #sound particle
-particle = copy.deepcopy(constrains_dict)
-particle_features = ['only_short_selected', 'very_short_scored', 'volume_hi',
-                     'less_eq', 'no_stretch', 'fade_in_short',
-                     'fade_out_mid']
-particel = get_constrains(particle_features)
+parameters['macro']['particle'] = copy.deepcopy(constrains_dict)
+particle_features = [p['selection_length']['short'], p['length']['very_short'],
+                    p['volume']['high'], p['eq']['less'], p['stretch']['never'],
+                    p['fade_in']['short'], p['fade_out']['mid']]
+parameters['macro']['particle'] = get_constrains(particle_features)
 
 
 #sound particle
-long_low = copy.deepcopy(constrains_dict)
-long_low_features = ['only_long_selected', 'very_long_scored', 'volume_mid',
-                     'less_eq', 'shift_low', 'fade_in_long',
-                     'fade_out_long']
-long_low = get_constrains(long_low_features)
+parameters['macro']['long_low'] = copy.deepcopy(constrains_dict)
+long_low_features = [p['selection_length']['long'], p['length']['very_long'],
+                    p['volume']['mid'], p['eq']['less'],p['shift']['low'],
+                    p['fade_in']['long'], p['fade_out']['long']]
+parameters['macro']['long_low'] = get_constrains(long_low_features)
+
+gen_random_macro()
