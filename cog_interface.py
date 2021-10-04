a2c3ed529c1911e42f527c22d8a453ab9d4e1000
ericguizzo <eric.guizzo@hotmail.it>
2021-10-04 23:44:38 +0200

    cog-ified

--- cog_interface.py
+++ cog_interface.py
@@ -1,6 +1,5 @@
 import os,sys
 sys.path.append("source")
-#os.chdir("source")
 import tempfile
 from pathlib import Path
 
@@ -12,23 +11,17 @@ import scene_constrains as sc
 import random
 import utility_functions as uf
 
-
 class GenDream(cog.Predictor):
     def setup(self):
-        """Load the modules"""
-        max_dur = 10
-        self.scene = Scene(main_dur = 60 * max_dur)
-        self.allocator = Allocator(server_shared_path='test', sr=44100,
-                            client_shared_path='test')
-        self.post = Postprocessing()
-        #scene.gen_macro()
+        '''setup func'''
+        #nothing to load here
 
-    @cog.input("type", type=str, default="dream", options=["episode","dream"], help="Type of file to generate. Episode is a single scenario, dream is a concatenation of episodes. For 'dream' only 'fast', 'max_num_sounds', 'length' and 'memories' have an effect")
-    @cog.input("length", type=int, default=600, help="Soundfile length in minutes")
-    @cog.input("fast", type=bool, default=True, help="If True disables most pitch shifting events to speed up generation")
+    @cog.input("type", type=str, default="episode", options=["episode","dream"], help="Type of file to generate. Episode is a single scenario, dream is a concatenation of episodes. For 'dream' only 'fast', 'max_num_sounds', 'length' and 'memories' have an effect")
+    @cog.input("length", type=int, default=60, help="Soundfile length in minutes")
+    @cog.input("fast", type=bool, default=True, help="If True disables the most resource-demanding processes to speed up generation")
     @cog.input("max_num_sounds", type=int, default=50, help="Maximum number of simultaneous sounds")
-    @cog.input("memories", type=list, default=['all'], help="Type of memories that can occur in the dream. List of strings. Options:")
-
+    @cog.input("memories_instrumental", type=str, default='all', help="Type of instrumental memories that can occur in the dream. List of strings. Options: 'all', 'africanPercs', 'ambient1', 'buchla', 'buchla2', 'classical', 'classical2', 'guitarAcoustic', 'guitarBaroque', 'jazz', 'organ', 'percsWar', 'percussions', 'pianoChill', 'pianoDreamy', 'pianoSmooth'")
+    @cog.input("memories_soundscape", type=str, default='all', help="Type of soundscape memories that can occur in the dream. List of strings. Options: 'all', airport', 'birdsStreet', 'forest', 'library', 'mixed', 'office', 'rain', 'sea', 'train', 'wind'")
     @cog.input("density", type=float, default=0.2, help="Density of sound events [range 0-1]")
     @cog.input("score_diversity", type=float, default=0.2, help="Diversity of sound archetye choice [range 0-1]")
     @cog.input("timbre_diversity", type=float, default=0.2, help="Diversity of chosen timbres")
@@ -38,17 +31,34 @@ class GenDream(cog.Predictor):
     @cog.input("complete_random", type=bool, default=False, help="If true everything is random despite what is selected in the UI")
     @cog.input("global_rev_amount", type=float, default=0.1, help="Amount of global reverb [range 0-1]")
     @cog.input("global_stretch", type=float, default=0., help="Amount of time stretching. Negative to shorten, positive to increase duration")
-    @cog.input("global_shift", type=float, default=-2000., help="Amount of pitch shifting. Negative to lower, positive to increase pitch")
+    @cog.input("global_shift", type=float, default=0., help="Amount of pitch shifting. Negative to lower, positive to increase pitch")
+    @cog.input("output_type", type=str, default='mp3', options=["wav", "mp3"], help="Wav or mp3 output")
 
-    def predict(self, type, length, max_num_sounds, density, score_diversity, timbre_diversity, fast, carpet,
+    def predict(self, type, length, fast, max_num_sounds, memories_instrumental,
+                memories_soundscape, output_type, density, score_diversity, timbre_diversity, carpet,
                     perc_particles, complete_random, enhance_random, global_rev_amount,
                     global_stretch, global_shift):
         """Compute dream!!"""
+        #init paths and classes
+        output_path_wav = Path(tempfile.mkdtemp()) / "output.wav"
+        output_path_mp3 = Path(tempfile.mkdtemp()) / "output.mp3"
         self.build = BuildScene(max_dur=60*length, max_num_sounds=max_num_sounds)
         self.dream = Dream(scene_maxdur=20, max_num_sounds=max_num_sounds)
+        self.post = Postprocessing()
 
-        choice_dict = {'instrumental': ['pianoDreamy']}
+        #create memories dict
+        if memories_instrumental == 'all':
+            memories_instrumental = sc.check_available_models()['instrumental']
+        else:
+            memories_instrumental = memories_instrumental.split(',')
+        if memories_soundscape == 'all':
+            memories_soundscape = sc.check_available_models()['fieldrec']
+        else:
+            memories_soundscape = memories_soundscape.split(',')
+        choice_dict = {'instrumental': memories_instrumental, 'fieldrec': memories_soundscape}
+        print ("Memories dict: ", choice_dict)
 
+        #set global proccesing bools
         if global_rev_amount == 0.0:
             global_rev = False
         else:
@@ -64,6 +74,7 @@ class GenDream(cog.Predictor):
         else:
             global_shift_dir = 0
 
+        #compute scene if desired
         if type == 'episode':
             if not complete_random:
                 mix, score = self.build.build(length=1,
@@ -71,8 +82,8 @@ class GenDream(cog.Predictor):
                                   score_diversity=score_diversity,
                                   sel_diversity=timbre_diversity,
                                   single_model=False,
-                                  fixed_category='fieldrec',
-                                  fixed_model='forest',
+                                  fixed_category=False,
+                                  fixed_model=False,
                                   neuro_choice=choice_dict,
                                   fast=fast,
                                   carpet=carpet,
@@ -89,10 +100,30 @@ class GenDream(cog.Predictor):
 
             else:
                 mix, score, p = self.build.random_build(length=1, neuro_choice=choice_dict)
+        #or compute dream
         else:
-            mix = dream.random_dream(length, neuro_choice=choice_dict)
+            mix = self.dream.random_dream(length, neuro_choice=choice_dict)
 
+        #cut silences longer than 3 secs
         mix = self.post.cut_silence_multichannel(mix)
-        self.allocator.write_local(mix, 'test')
-        self.scene.load_score(score)
-        #self.scene.plot_score(dimensions=3)
+
+        #write output to file
+        print ('Writing sounds to file')
+        swapped_sound = np.swapaxes(mix,0,1)
+        soundfile.write(str(output_path_wav), swapped_sound, 44100, format='WAV', subtype='PCM_16')
+
+        #convert to mp3 if desired
+        if output_type == "mp3":
+            subprocess.check_output(
+                [
+                    "ffmpeg",
+                    "-i",
+                    str(output_path_wav),
+                    "-af",
+                    "silenceremove=1:0:-50dB,aformat=dblp,areverse,silenceremove=1:0:-50dB,aformat=dblp,areverse",  # strip silence
+                    str(output_path_mp3),
+                ],
+            )
+            return output_path_mp3
+        else:
+            return output_path_wav
